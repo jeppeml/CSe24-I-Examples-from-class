7bd4705267859c122eadc41a18d8d93141805a82
jeppeml <jeppeml@users.noreply.github.com>
2024-09-23 12:26:11 +0200

    .

---
+++ IntelliJ/1stProgram/src/main/java/dk/easv/Main.java
@@ -0,0 +1,9 @@
+package dk.easv;
+
+public class Main {
+    public static void main(String[] arg) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world!");
+        }
+    }
+}
---
+++ IntelliJ/FirstJavaFX/src/main/java/dk/easv/firstjavafx/HelloController.java
@@ -0,0 +1,14 @@
+package dk.easv.firstjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Screw you guys, I'm going home!  - Cartman");
+    }
+}
