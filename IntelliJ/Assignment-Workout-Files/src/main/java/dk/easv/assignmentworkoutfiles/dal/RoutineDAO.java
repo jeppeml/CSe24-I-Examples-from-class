d749318287afdb1fd4a1559c335beef4548a0a33
jeppeml <jeppeml@users.noreply.github.com>
2024-11-14 10:04:47 +0100

    removed some dead code

--- IntelliJ/Assignment-Workout-Files/src/main/java/dk/easv/assignmentworkoutfiles/dal/RoutineDAO.java
+++ IntelliJ/Assignment-Workout-Files/src/main/java/dk/easv/assignmentworkoutfiles/dal/RoutineDAO.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.text.spi.DateFormatProvider;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
--- IntelliJ/Assignment-Workout-Files/src/main/java/dk/easv/assignmentworkoutfiles/gui/controllers/WorkoutAppController.java
+++ IntelliJ/Assignment-Workout-Files/src/main/java/dk/easv/assignmentworkoutfiles/gui/controllers/WorkoutAppController.java
@@ -9,10 +9,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
